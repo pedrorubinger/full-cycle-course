8bcf7fb9ca8cc21c85ab255b0b90792bf031bd6b
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-09-13 21:01:27 -0300

    chore: add initial structure

--- docker-challenge-02/app/index.js
+++ docker-challenge-02/app/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const app = express()
 const port = 3000
+
+const connectDatabase = async () => {
+  const config = {
+    host: "db",
+    user: "root",
+    password: "pedro",
+    port: 3306
+  }
+  const connection = mysql.createConnection(config)
+
+  connection.connect((err) => {
+    if (err) console.log("An error occurred while trying to connect to the database:", err)
+    else console.log("Database connected successfully!")
+  })
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Full Cycle rocks!</h1>')
@@ -9,4 +25,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
+  console.log("Trying to connect to the database...")
+
+  setTimeout(() => {
+    connectDatabase()
+  }, 6000)
 })
